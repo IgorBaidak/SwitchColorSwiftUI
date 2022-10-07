6680c526ac08e2c5cfe3d1286d1a187e5841feea
IgorBaidak <igorbaidak@gmail.com>
2022-10-07 18:17:23 +0300

    Start

---
+++ SwitchColorSwiftUI/SwitchColorSwiftUI/SwitchColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchColorSwiftUIApp.swift
+//  SwitchColorSwiftUI
+//
+//  Created by Igor Baidak on 7.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SwitchColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
